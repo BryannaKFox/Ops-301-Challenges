32b147d570a2d6fda4e2c2044ab8b406b5b7c56f
BryannaKFox <146906807+BryannaKFox@users.noreply.github.com>
2023-12-07 18:21:53 -0800

    Conditional statement

---
+++ Conditional_statments.py
@@ -0,0 +1,32 @@
+# Script:                       ops301d10-challenge 09
+# Author:                       Bryanna Fox
+# Date of latest revision:      12/07/2023
+# Purpose:                      Conditional Statements
+
+# Values
+a = 5
+b = 10
+
+# If statement using logical conditionals
+if a == b:
+    print("a is equal to b.")
+elif a < b:
+    print("a is less than b.")
+else:
+    print("a is greater than b.")
+
+# Values
+x = 15
+y = 15
+
+# If statement with additional conditions
+if x == y:
+    print("x is equal to y.")
+elif x > y:
+    print("x is greater than y.")
+elif x < y:
+    print("x is less than y.")
+else:
+    print("This should never happen!")
+
+
